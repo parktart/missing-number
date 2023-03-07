a2b7a160ac7242c082a33c7157809c380ce3c5ce
parker <parkerschellenberg@gmail.com>
2023-03-07 16:29:19 -0600

    Define html elements in js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const input_arrayLength = document.querySelector('.array-length input');
+const button_makeArray = document.querySelector('#make-array');
+const div_arrayContainer = document.querySelector('.array-container.wrap');
+const div_randomContainer = document.querySelector('.random-button.wrap');
+const button_go = document.querySelector('.random-button button');
+const div_explainContainer = document.querySelector('.explain-container.wrap');
+const button_continue = document.querySelector('#continue');
+const div_missingContainer = document.querySelector('.missing-container.wrap');
+
